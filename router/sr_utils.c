f2cf0643d020cd7590328354f85435aac489b810
Jacky Chen <jackychencw@gmail.com>
2019-10-14 18:29:16 -0400

    fix bug:

--- router/sr_utils.c
+++ router/sr_utils.c
@@ -195,19 +195,3 @@ void print_hdrs(uint8_t *buf, uint32_t length)
     fprintf(stderr, "Unrecognized Ethernet Type: %d\n", ethtype);
   }
 }
-
-sr_arp_hdr_t *get_arp_hdr(uint8_t *buf)
-{
-  return (sr_arp_hdr_t *)(buf + sizeof(sr_ethernet_hdr_t));
-}
-
-sr_ethernet_hdr_t *get_ethernet_hdr(uint8_t *buf)
-{
-  return (sr_ethernet_hdr_t *)(buf + sizeof(sr_ethernet_hdr_t));
-};
-
-uint8_t arp_sanity_check(unsigned int len)
-{
-  int min_len = sizeof(sr_ethernet_hdr_t) + sizeof(sr_arp_hdr_t);
-  return len >= min_len;
-}
