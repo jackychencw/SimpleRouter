f82cbc7a8940285c1c9acceb336eee3c22a4d166
Jacky Chen <jackychencw@gmail.com>
2019-10-14 04:01:25 -0400

    fix typo

--- router/sr_utils.c
+++ router/sr_utils.c
@@ -198,15 +198,15 @@ void print_hdrs(uint8_t *buf, uint32_t length)
 
 sr_arp_hdr_t *get_arp_hdr(uint8_t *buf)
 {
-  return (sr_arp_hdr_t *)(buf + sizeof(sr_arp_hdr_t));
+  return (sr_arp_hdr_t *)(buf);
 }
 
 sr_ethernet_hdr_t *get_ethernet_hdr(uint8_t *buf)
 {
-  return (sr_eth_hdr_t *)(buf + sizeof(st_ethernet_hdr_t));
+  return (sr_ethernet_hdr_t *)(buf);
 };
 
-bool arp_sanity_check(unsigned int len)
+uint8_t arp_sanity_check(unsigned int len)
 {
   int minlength = sizeof(sr_ethernet_hdr_t) + sizeof(sr_arp_hdr_t);
   return len >= minlength;
