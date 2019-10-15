91815687f81efcd6df5257b8e06b665026a22700
Jacky Chen <jackychencw@gmail.com>
2019-10-15 12:17:23 -0400

    fix infinite loop

--- router/sr_rt.c
+++ router/sr_rt.c
@@ -194,6 +194,7 @@ struct sr_rt *sr_rt_lpm_lookup(struct sr_instance *sr, struct in_addr dest)
             lpm = rt;
             lpm_len = length;
         }
+        rt = rt->next;
     }
     /* No interface found. */
     return lpm;
