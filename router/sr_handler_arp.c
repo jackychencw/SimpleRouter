5a360ec28a93f19890d41780b16dadc55ed1b8b7
Jacky Chen <jackychencw@gmail.com>
2019-10-15 22:00:40 -0400

    add

--- router/sr_handler_arp.c
+++ router/sr_handler_arp.c
@@ -70,7 +70,7 @@ uint8_t *create_arp_packet(uint8_t *sha, uint32_t sip, uint8_t *tha, uint32_t ti
     arp_hder->ar_sip = sip;
     memcpy(arp_hder->ar_tha, tha, ETHER_ADDR_LEN);
     arp_hder->ar_tip = tip;
-    print("done\n");
+    printf("done\n");
 
     return packet;
 }
